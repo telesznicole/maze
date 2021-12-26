7af4bbbb5efd70ae30d9d00b613513fef76ffa37
Nicole Telesz <58489255+telesznicole@users.noreply.github.com>
2021-12-26 18:42:44 -0500

    uploaded additional files

---
+++ cmake-build-debug/CMakeFiles/maze.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nicoletelesz/CLionProjects/maze/loadImage.cpp" "/Users/nicoletelesz/CLionProjects/maze/cmake-build-debug/CMakeFiles/maze.dir/loadImage.cpp.o"
+  "/Users/nicoletelesz/CLionProjects/maze/main.cpp" "/Users/nicoletelesz/CLionProjects/maze/cmake-build-debug/CMakeFiles/maze.dir/main.cpp.o"
+  "/Users/nicoletelesz/CLionProjects/maze/player.cpp" "/Users/nicoletelesz/CLionProjects/maze/cmake-build-debug/CMakeFiles/maze.dir/player.cpp.o"
+  "/Users/nicoletelesz/CLionProjects/maze/tile.cpp" "/Users/nicoletelesz/CLionProjects/maze/cmake-build-debug/CMakeFiles/maze.dir/tile.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/maze.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maze.dir/loadImage.cpp.o"
+  "CMakeFiles/maze.dir/main.cpp.o"
+  "CMakeFiles/maze.dir/player.cpp.o"
+  "CMakeFiles/maze.dir/tile.cpp.o"
+  "maze"
+  "maze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
